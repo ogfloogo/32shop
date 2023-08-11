d0b1b86f8f86a401f3b3a2a03a38d2d1ed98c120
h <472131863@qq.com>
2023-08-11 15:56:09 +0800

    1

--- application/admin/controller/Dashboard.php
+++ application/admin/controller/Dashboard.php
@@ -3,11 +3,21 @@
 namespace app\admin\controller;
 
 use app\admin\model\Admin;
+use app\admin\model\finance\UserCash;
+use app\admin\model\finance\UserRecharge;
+use app\admin\model\financebuy\FinanceOrder;
+use app\admin\model\order\Order;
+use app\admin\model\sys\Report;
+use app\admin\model\sys\TeamLevel;
 use app\admin\model\User;
+use app\admin\model\userlevel\UserLevel;
+use app\api\model\Usertotal;
 use app\common\controller\Backend;
+use app\common\library\Log;
 use app\common\model\Attachment;
 use fast\Date;
 use think\Db;
+use think\Log as ThinkLog;
 
 /**
  * 控制台
@@ -26,59 +36,173 @@ class Dashboard extends Backend
         try {
             \think\Db::execute("SET @@sql_mode='';");
         } catch (\Exception $e) {
-
         }
         $column = [];
-        $starttime = Date::unixtime('day', -6);
+        $starttime = Date::unixtime('day', -30);
         $endtime = Date::unixtime('day', 0, 'end');
-        $joinlist = Db("user")->where('jointime', 'between time', [$starttime, $endtime])
-            ->field('jointime, status, COUNT(*) AS nums, DATE_FORMAT(FROM_UNIXTIME(jointime), "%Y-%m-%d") AS join_date')
-            ->group('join_date')
-            ->select();
+        // $joinlist = Db("user")->where('jointime', 'between time', [$starttime, $endtime])
+        //     ->field('jointime, status, COUNT(*) AS nums, DATE_FORMAT(FROM_UNIXTIME(jointime), "%Y-%m-%d") AS join_date')
+        //     ->group('join_date')
+        //     ->select();
         for ($time = $starttime; $time <= $endtime;) {
-            $column[] = date("Y-m-d", $time);
+            $date = date("Y-m-d", $time);
+            $column[] = date("m-d", $time);
+            $rechargeList[] = (new \app\admin\model\Statistics())->where(['date' => $date])->sum('turnover');
+//            $time_range = ['between', [strtotime($date . ' 00:00:00'), strtotime($date . ' 23:59:59')]];
+//            $reportInfo = (new Report())->where(['date' => $date])->find();
+//
+//            $rechargeUserList[] = !empty($reportInfo) ? $reportInfo['rechargeuser'] : 0;
+//            $firstRechargeUserList[] = !empty($reportInfo) ? $reportInfo['first_rechargeuser'] : 0;
+//            $rechargeList[] = (new UserRecharge())->where(['status' => 1, 'createtime' => $time_range])->sum('price');
+//            $withdrawList[] = (new UserCash())->where(['status' => 2, 'createtime' => $time_range])->sum('price');
+//            $userList[] = (new User())->where(['createtime' => $time_range, 'is_robot' => 0])->count();
+//            $loginList[] = intval((new Usertotal())->getLoginCount(date('ymd', $time)));
             $time += 86400;
         }
-        $userlist = array_fill_keys($column, 0);
-        foreach ($joinlist as $k => $v) {
-            $userlist[$v['join_date']] = $v['nums'];
-        }
 
-        $dbTableList = Db::query("SHOW TABLE STATUS");
-        $addonList = get_addon_list();
-        $totalworkingaddon = 0;
-        $totaladdon = count($addonList);
-        foreach ($addonList as $index => $item) {
-            if ($item['state']) {
-                $totalworkingaddon += 1;
-            }
+        $goods = (new \app\admin\model\Goods())->order('sales desc')->limit(10)->select();
+        foreach ($goods as $v){
+            $goods_name[] = $v['name'];
+            $sales[] = $v['sales'];
         }
+//
+//        $levelList = (new TeamLevel())->order('level ASC')->column('level');
+//        foreach ($levelList as $level) {
+//            $userLevelList[] = (new User())->where(['buy_level' => $level, 'is_robot' => 0, 'status' => 1])->count();
+//        }
+//        $nosid_user = User::where(['sid' => 0])->column('id');
+//        $step = 1000;
+//        $today_nosid_recharge = 0;
+//        if (count($nosid_user) > $step) {
+//            $num = count($nosid_user);
+//            for ($i = 0; $i < ceil($num / $step); $i++) {
+//                $step_nosid_user = array_slice($nosid_user, $i * $step, $step);
+//                $today_nosid_recharge += UserRecharge::where(['status' => 1, 'user_id' => ['IN', $step_nosid_user], 'createtime' => ['between', [strtotime(date('Y-m-d') . ' 00:00:00'), strtotime(date('Y-m-d') . ' 23:59:59')]]])->sum('price');
+//            }
+//        }
+//        $total_recharge = (new UserRecharge())->where(['status' => 1])->distinct(true)->field('user_id')->select();
+//        $total_withdraw = (new UserCash())->where(['id' => ['GT', 0]])->distinct(true)->field('user_id')->select();
+//
+//        $todayReport = (new Report())->where(['date' => date('Y-m-d')])->find();
+//        $todayLogin = (new Usertotal())->getLoginCount();
+
         $this->view->assign([
-            'totaluser'         => User::count(),
-            'totaladdon'        => $totaladdon,
-            'totaladmin'        => Admin::count(),
-            'totalcategory'     => \app\common\model\Category::count(),
-            'todayusersignup'   => User::whereTime('jointime', 'today')->count(),
-            'todayuserlogin'    => User::whereTime('logintime', 'today')->count(),
-            'sevendau'          => User::whereTime('jointime|logintime|prevtime', '-7 days')->count(),
-            'thirtydau'         => User::whereTime('jointime|logintime|prevtime', '-30 days')->count(),
-            'threednu'          => User::whereTime('jointime', '-3 days')->count(),
-            'sevendnu'          => User::whereTime('jointime', '-7 days')->count(),
-            'dbtablenums'       => count($dbTableList),
-            'dbsize'            => array_sum(array_map(function ($item) {
-                return $item['Data_length'] + $item['Index_length'];
-            }, $dbTableList)),
-            'totalworkingaddon' => $totalworkingaddon,
-            'attachmentnums'    => Attachment::count(),
-            'attachmentsize'    => Attachment::sum('filesize'),
-            'picturenums'       => Attachment::where('mimetype', 'like', 'image/%')->count(),
-            'picturesize'       => Attachment::where('mimetype', 'like', 'image/%')->sum('filesize'),
+
+            'totaluser'         => (new \app\admin\model\Monthstatistics())->sum('money'),
+            'totalorder'        => (new \app\admin\model\Monthstatistics())->where(['year'=>date('Y'),'month'=>date('m')])->value('money'),
+            'totalrecharge'        => (new \app\admin\model\Goods())->sum('sales'),
+//            'totalwithdraw'     => UserCash::where(['status' => ['IN', [2, 3]]])->sum('price'),
+//
+//            'todaynewuser'    => isset($todayReport['user']) ? $todayReport['user'] : 0,
+//            'todaylogin'    => intval($todayLogin),
+//
+//            'todayorder'        => FinanceOrder::where(['is_robot' => 0, 'createtime' => ['between', [strtotime(date('Y-m-d') . ' 00:00:00'), strtotime(date('Y-m-d') . ' 23:59:59')]]])->count(),
+//            'todayopenorder'    => Order::where(['pay_status' => 1, 'type' => 1, 'createtime' => ['between', [strtotime(date('Y-m-d') . ' 00:00:00'), strtotime(date('Y-m-d') . ' 23:59:59')]]])->count(),
+//            'todayordermoney'        => FinanceOrder::where(['is_robot' => 0, 'createtime' => ['between', [strtotime(date('Y-m-d') . ' 00:00:00'), strtotime(date('Y-m-d') . ' 23:59:59')]]])->sum('amount'),
+//
+//
+//            'todayrecharge'        => UserRecharge::where(['status' => 1, 'updatetime' => ['between', [strtotime(date('Y-m-d') . ' 00:00:00'), strtotime(date('Y-m-d') . ' 23:59:59')]]])->sum('price'),
+//            'todaywithdraw'     => UserCash::where(['status' => ['IN', [2, 3]], 'updatetime' => ['between', [strtotime(date('Y-m-d') . ' 00:00:00'), strtotime(date('Y-m-d') . ' 23:59:59')]]])->sum('price'),
+//
+//            'todaywithdrawnum'     => UserCash::where(['status' => 0, 'createtime' => ['between', [strtotime(date('Y-m-d') . ' 00:00:00'), strtotime(date('Y-m-d') . ' 23:59:59')]]])->count(),
+//            'todaywithdrawmoney'     => UserCash::where(['status' => 0, 'createtime' => ['between', [strtotime(date('Y-m-d') . ' 00:00:00'), strtotime(date('Y-m-d') . ' 23:59:59')]]])->sum('price'),
+//
+//            'today_nosid_newuser'         => User::where(['sid' => 0, 'createtime' => ['between', [strtotime(date('Y-m-d') . ' 00:00:00'), strtotime(date('Y-m-d') . ' 23:59:59')]]])->count(),
+//            'today_nosid_recharge'        => $today_nosid_recharge,
+//
+//            'totaluserrecharge'         => count($total_recharge),
+//            'totaluserwithdraw'         => count($total_withdraw),
+
         ]);
 
-        $this->assignconfig('column', array_keys($userlist));
-        $this->assignconfig('userdata', array_values($userlist));
-
+        $this->assignconfig('chart_data', ['date' => $column,'re_user' => $rechargeList,'goods_name' => $goods_name,'sales' => $sales
+//             'withdraw' => $withdrawList, 'user' => $loginList, 'reg' => $userList, 're_user' => $rechargeUserList, 'first' => $firstRechargeUserList
+        ]);
+//        $this->assignconfig('level_data', ['level' => $levelList, 'user' => $userLevelList]);
+//        $this->getAgentData();
+//        $this->getAgentDataList();
         return $this->view->fetch();
     }
 
+    protected function getAgentData()
+    {
+        $total_recharge = (new UserRecharge())->where(['status' => 1, 'agent_id' => ['GT', 0]])->distinct(true)->field('user_id')->select();
+        $total_withdraw = (new UserCash())->where(['id' => ['GT', 0], 'agent_id' => ['GT', 0]])->distinct(true)->field('user_id')->select();
+
+        $agent_info = [
+
+            'totaluser'         => User::where(['agent_id' => ['GT', 0]])->count(),
+            'totalorder'        => Order::where(['pay_status' => 1, 'agent_id' => ['GT', 0]])->count(),
+            'totalrecharge'        => UserRecharge::where(['status' => 1, 'agent_id' => ['GT', 0]])->sum('price'),
+            'totalwithdraw'     => UserCash::where(['status' => ['IN', [2, 3]], 'agent_id' => ['GT', 0]])->sum('price'),
+
+            'todaynewuser'    => User::where(['agent_id' => ['GT', 0], 'createtime' => ['between', [strtotime(date('Y-m-d') . ' 00:00:00'), strtotime(date('Y-m-d') . ' 23:59:59')]]])->count(),
+            'todaylogin'    => User::where(['agent_id' => ['GT', 0], 'logintime' => ['between', [strtotime(date('Y-m-d') . ' 00:00:00'), strtotime(date('Y-m-d') . ' 23:59:59')]]])->count(),
+
+            'todayorder'        => Order::where(['pay_status' => 1, 'agent_id' => ['GT', 0], 'createtime' => ['between', [strtotime(date('Y-m-d') . ' 00:00:00'), strtotime(date('Y-m-d') . ' 23:59:59')]]])->count(),
+            'todayopenorder'    => Order::where(['pay_status' => 1, 'agent_id' => ['GT', 0], 'type' => 1, 'createtime' => ['between', [strtotime(date('Y-m-d') . ' 00:00:00'), strtotime(date('Y-m-d') . ' 23:59:59')]]])->count(),
+
+
+            'todayrecharge'        => UserRecharge::where(['status' => 1, 'agent_id' => ['GT', 0], 'updatetime' => ['between', [strtotime(date('Y-m-d') . ' 00:00:00'), strtotime(date('Y-m-d') . ' 23:59:59')]]])->sum('price'),
+            'todaywithdraw'     => UserCash::where(['status' => ['IN', [2, 3]], 'agent_id' => ['GT', 0], 'updatetime' => ['between', [strtotime(date('Y-m-d') . ' 00:00:00'), strtotime(date('Y-m-d') . ' 23:59:59')]]])->sum('price'),
+
+            'todaywithdrawnum'     => UserCash::where(['status' => 0, 'agent_id' => ['GT', 0], 'createtime' => ['between', [strtotime(date('Y-m-d') . ' 00:00:00'), strtotime(date('Y-m-d') . ' 23:59:59')]]])->count(),
+            'todaywithdrawmoney'     => UserCash::where(['status' => 0, 'agent_id' => ['GT', 0], 'createtime' => ['between', [strtotime(date('Y-m-d') . ' 00:00:00'), strtotime(date('Y-m-d') . ' 23:59:59')]]])->sum('price'),
+
+            'today_nosid_newuser'         => User::where(['sid' => 0, 'agent_id' => ['GT', 0], 'createtime' => ['between', [strtotime(date('Y-m-d') . ' 00:00:00'), strtotime(date('Y-m-d') . ' 23:59:59')]]])->count(),
+            'today_nosid_recharge'        => 0,
+
+            'totaluserrecharge'         => count($total_recharge),
+            'totaluserwithdraw'         => count($total_withdraw),
+
+        ];
+        $this->view->assign('agent', $agent_info);
+    }
+
+    protected function getAgentDataById($agent_id)
+    {
+        $total_recharge = (new UserRecharge())->where(['status' => 1, 'agent_id' => $agent_id])->distinct(true)->field('user_id')->select();
+        $total_withdraw = (new UserCash())->where(['id' => ['GT', 0], 'agent_id' => $agent_id])->distinct(true)->field('user_id')->select();
+
+        $agent_info = [
+
+            'totaluser'         => User::where(['agent_id' => $agent_id])->count(),
+            'totalorder'        => Order::where(['pay_status' => 1, 'agent_id' => $agent_id])->count(),
+            'totalrecharge'        => UserRecharge::where(['status' => 1, 'agent_id' => $agent_id])->sum('price'),
+            'totalwithdraw'     => UserCash::where(['status' => ['IN', [2, 3]], 'agent_id' => $agent_id])->sum('price'),
+
+            'todaynewuser'    => User::where(['agent_id' => $agent_id, 'createtime' => ['between', [strtotime(date('Y-m-d') . ' 00:00:00'), strtotime(date('Y-m-d') . ' 23:59:59')]]])->count(),
+            'todaylogin'    => User::where(['agent_id' => $agent_id, 'logintime' => ['between', [strtotime(date('Y-m-d') . ' 00:00:00'), strtotime(date('Y-m-d') . ' 23:59:59')]]])->count(),
+
+            'todayorder'        => Order::where(['pay_status' => 1, 'agent_id' => $agent_id, 'createtime' => ['between', [strtotime(date('Y-m-d') . ' 00:00:00'), strtotime(date('Y-m-d') . ' 23:59:59')]]])->count(),
+            'todayopenorder'    => Order::where(['pay_status' => 1, 'agent_id' => $agent_id, 'type' => 1, 'createtime' => ['between', [strtotime(date('Y-m-d') . ' 00:00:00'), strtotime(date('Y-m-d') . ' 23:59:59')]]])->count(),
+
+
+            'todayrecharge'        => UserRecharge::where(['status' => 1, 'agent_id' => $agent_id, 'updatetime' => ['between', [strtotime(date('Y-m-d') . ' 00:00:00'), strtotime(date('Y-m-d') . ' 23:59:59')]]])->sum('price'),
+            'todaywithdraw'     => UserCash::where(['status' => ['IN', [2, 3]], 'agent_id' => $agent_id, 'updatetime' => ['between', [strtotime(date('Y-m-d') . ' 00:00:00'), strtotime(date('Y-m-d') . ' 23:59:59')]]])->sum('price'),
+
+            'todaywithdrawnum'     => UserCash::where(['status' => 0, 'agent_id' => $agent_id, 'createtime' => ['between', [strtotime(date('Y-m-d') . ' 00:00:00'), strtotime(date('Y-m-d') . ' 23:59:59')]]])->count(),
+            'todaywithdrawmoney'     => UserCash::where(['status' => 0, 'agent_id' => $agent_id, 'createtime' => ['between', [strtotime(date('Y-m-d') . ' 00:00:00'), strtotime(date('Y-m-d') . ' 23:59:59')]]])->sum('price'),
+
+            'today_nosid_newuser'         => User::where(['sid' => 0, 'agent_id' => $agent_id, 'createtime' => ['between', [strtotime(date('Y-m-d') . ' 00:00:00'), strtotime(date('Y-m-d') . ' 23:59:59')]]])->count(),
+            'today_nosid_recharge'        => 0,
+
+            'totaluserrecharge'         => count($total_recharge),
+            'totaluserwithdraw'         => count($total_withdraw),
+
+        ];
+        return $agent_info;
+    }
+
+    protected function getAgentDataList()
+    {
+        $assignList = [];
+        $list = (new Admin())->where(['agent_id' => ['GT', 0], 'status' => 'normal'])->field('username,agent_id')->select();
+        if (count($list)) {
+            foreach ($list as $item) {
+                $assignList[$item['username']] = $this->getAgentDataById($item['agent_id']);
+            }
+        }
+        $this->view->assign('assignList', $assignList);
+    }
 }
--- public/assets/js/backend/dashboard.js
+++ public/assets/js/backend/dashboard.js
@@ -1,75 +1,215 @@
+const { data } = require("jquery");
+
 define(['jquery', 'bootstrap', 'backend', 'addtabs', 'table', 'echarts', 'echarts-theme', 'template'], function ($, undefined, Backend, Datatable, Table, Echarts, undefined, Template) {
 
     var Controller = {
         index: function () {
             // 基于准备好的dom，初始化echarts实例
-            var myChart = Echarts.init(document.getElementById('echart'), 'walden');
+            var chartCash = Echarts.init(document.getElementById('echart-cash'), 'walden');
+            var chartReg = Echarts.init(document.getElementById('echart-reg'), 'walden');
+            // var chartUser = Echarts.init(document.getElementById('echart-user'), 'walden');
+            console.log(Config.chart_data.date);
 
-            // 指定图表的配置项和数据
-            var option = {
+            var option1 = {
                 title: {
-                    text: '',
-                    subtext: ''
+                    text: '近30日销售额'
                 },
-                color: [
-                    "#18d1b1",
-                    "#3fb1e3",
-                    "#626c91",
-                    "#a0a7e6",
-                    "#c4ebad",
-                    "#96dee8"
-                ],
                 tooltip: {
-                    trigger: 'axis'
-                },
-                legend: {
-                    data: [__('Register user')]
-                },
-                toolbox: {
-                    show: false,
-                    feature: {
-                        magicType: {show: true, type: ['stack', 'tiled']},
-                        saveAsImage: {show: true}
+                    trigger: 'axis',
+                    axisPointer: {
+                        type: 'shadow'
                     }
                 },
-                xAxis: {
-                    type: 'category',
-                    boundaryGap: false,
-                    data: Config.column
+                legend: {
+                    top: '10%'
                 },
-                yAxis: {},
-                grid: [{
-                    left: 'left',
-                    top: 'top',
-                    right: '10',
-                    bottom: 30
-                }],
-                series: [{
-                    name: __('Register user'),
-                    type: 'line',
-                    smooth: true,
-                    areaStyle: {
-                        normal: {}
-                    },
-                    lineStyle: {
-                        normal: {
-                            width: 1.5
+                grid: {
+                    top: '20%',
+                    left: '10%',
+                    right: '4%',
+                    bottom: '3%',
+                    containLabel: true
+                },
+                xAxis: [
+                    {
+                        type: 'category',
+                        data: Config.chart_data.date,
+                        splitLine: { show: false },
+
+                        axisTick: {
+                            alignWithLabel: true
                         }
+                    }
+                ],
+                yAxis: [
+                    {
+                        type: 'value',
+                        boundaryGap: [0, 0.01]
+                    }
+                ],
+                series: [
+                    // {
+                    //     name: '充值金额',
+                    //     type: 'bar',
+
+                    //     label: {
+                    //         show: true,
+                    //         position: 'top'
+                    //     },
+                    //     data: Config.chart_data.recharge,
+                    //     color: 'rgb(75,134,232)',
+                    // },
+                    // {
+                    //     name: '提现金额',
+                    //     type: 'bar',
+                    //     color: 'rgb(94,187,195)',
+
+                    //     label: {
+                    //         show: true,
+                    //         position: 'top'
+                    //     },
+                    //     data: Config.chart_data.withdraw
+                    // }
+                    {
+                        name: '销售额',
+                        type: 'bar',
+
+                        label: {
+                            show: true,
+                            position: 'top'
+                        },
+                        data: Config.chart_data.re_user,
+                        color: 'rgb(75,134,232)',
                     },
-                    data: Config.userdata
-                }]
+                    // {
+                    //     name: '首充人数',
+                    //     type: 'bar',
+                    //     color: 'rgb(94,187,195)',
+                    //
+                    //     label: {
+                    //         show: true,
+                    //         position: 'top'
+                    //     },
+                    //     data: Config.chart_data.first
+                    // }
+                ]
             };
 
             // 使用刚指定的配置项和数据显示图表。
-            myChart.setOption(option);
+            chartCash.setOption(option1);
+            var option2 = {
+                title: {
+                    text: '热销商品排行'
+                },
+                tooltip: {
+                    trigger: 'axis',
+                    axisPointer: {
+                        type: 'shadow'
+                    }
+                },
+                legend: {
+                    top: '10%'
+                },
+                grid: {
+                    top: '20%',
+                    left: '10%',
+                    right: '4%',
+                    bottom: '3%',
+                    containLabel: true
+                },
+                xAxis: [
+                    {
+                        type: 'category',
+                        data: Config.chart_data.goods_name,
+                        splitLine: { show: false },
+
+                        axisTick: {
+                            alignWithLabel: true
+                        }
+                    }
+                ],
+                yAxis: [
+                    {
+                        type: 'value',
+                        boundaryGap: [0, 0.01]
+                    }
+                ],
+                series: [
+                    {
+                        name: '销量',
+                        type: 'bar',
+                        data: Config.chart_data.sales,
+                        color: 'rgb(222,117,94)',
+                        label: {
+                            show: true,
+                            position: 'top'
+                        },
+                    },
+
+                ]
+            };
+            chartReg.setOption(option2);
+            // var option3 = {
+            //     title: {
+            //         text: '会员等级人数统计'
+            //     },
+            //     tooltip: {
+            //         trigger: 'axis',
+            //         axisPointer: {
+            //             type: 'shadow'
+            //         }
+            //     },
+            //
+            //     grid: {
+            //         left: '3%',
+            //         right: '4%',
+            //         bottom: '3%',
+            //         containLabel: true
+            //     },
+            //     xAxis: [
+            //         {
+            //             type: 'category',
+            //             splitLine: { show: false },
+            //
+            //             data: Config.level_data.level,
+            //             axisTick: {
+            //                 alignWithLabel: true
+            //             }
+            //         }
+            //     ],
+            //     yAxis: [
+            //         {
+            //             type: 'value',
+            //
+            //         }
+            //     ],
+            //     series: [
+            //         {
+            //             name: '会员人数',
+            //             type: 'bar',
+            //             data: Config.level_data.user,
+            //             color: 'rgb(75,134,232)',
+            //             barWidth: '30%',
+            //             label: {
+            //                 show: true,
+            //                 position: 'top'
+            //             },
+            //         }
+            //     ]
+            // };
+            // chartUser.setOption(option3);
 
             $(window).resize(function () {
-                myChart.resize();
+                chartCash.resize();
+                chartReg.resize();
+                chartUser.resize();
             });
 
             $(document).on("click", ".btn-refresh", function () {
                 setTimeout(function () {
-                    myChart.resize();
+                    chartCash.resize();
+                    chartReg.resize();
+                    // chartUser.resize();
                 }, 0);
             });
 
